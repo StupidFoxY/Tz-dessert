475ef41f30856674e790d974a3ee46568beccfee
liyanyu <liyanyu@cn.cogenda.com>
2024-06-19 10:32:47 +0800

    remove code

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -10,18 +10,7 @@ const globalStore = GlobalStore();
 console.log("%c Line:6 🍋 globalStore", "color:#ffffff;background:#ed9ec7", globalStore.shoppingCartItems);
 
 onBeforeMount(() => {
-    // let params:string[] = [];
-    // for(let key in globalStore.shoppingCartItems){
-    //     params.push(key)
-    // }
-    // sweetApi.getSweetDeatil(params).then((result:any)=>{
-    //     if(result.success){
-    //         console.log("%c Line:15 🍣 result", "color:#ffffff;background:#2eafb0", result);
-            
-    //     }else{
-    //         message.error(result.message)
-    //     }
-    // })
+    
 })
 </script>
 <style scoped></style>
